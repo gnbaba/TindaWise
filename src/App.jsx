b909c3d84ff4dbabc14837ddda25a6fc83ed3831
iPedProMax <ecaampz@gmail.com>
2025-09-29 19:45:12 +0800

    Dashboard with Sidebar and header

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Dashboard from './home/Dashboard'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Dashboard />}/>
       </Routes>
     </Router>
   );
